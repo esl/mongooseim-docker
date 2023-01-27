5b95a1cc45b79b5832bd897f8328d6a1611370ce
Mikhail Uvarov <arcusfelis@gmail.com>
2023-01-27 12:56:31 +0100

    Make bootstrap optional
    Use newer api to register users

--- smoke_test.sh
+++ smoke_test.sh
@@ -5,7 +5,7 @@ IMAGE=${IMAGE:-mongooseim}
 
 echo "Start MongooseIM container"
 docker rm -f mongooseim-smoke
-docker run --name=mongooseim-smoke -e JOIN_CLUSTER=false -d mongooseim
+docker run --name=mongooseim-smoke -e JOIN_CLUSTER=false -e BOOTSTRAP_ENABLED=true -d mongooseim
 
 CTL="docker exec mongooseim-smoke mongooseimctl"
 
@@ -15,8 +15,8 @@ $CTL started
 echo "Checking status via 'mongooseimctl status'"
 $CTL status
 
-echo "Trying to register a user with 'mongooseimctl register localhost a_password'"
-$CTL register localhost a_password
+echo "Trying to register a user"
+$CTL account registerUser --domain localhost --password a_password
 
 echo "Check if bootstap script works"
 docker logs mongooseim-smoke | grep "Hello from"
